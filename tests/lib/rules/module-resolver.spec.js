1ba3423b9c511f4278fa5a2985ba1361a4a6bc3a
Jordan Tepper <jstepper91@gmail.com>
2018-04-30 10:12:00 -0400

    Initial tests

--- tests/lib/rules/module-resolver.spec.js
+++ tests/lib/rules/module-resolver.spec.js
@@ -1,30 +1,33 @@
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
+const rule = require('../../../lib/rules/module-resolver')
+const { RuleTester } = require('eslint')
 
-const rule = require("../../../lib/rules/module-resolver")
-const { RuleTester } = require("eslint")
+// Change working directory so closest .babelrc is the one in tests/
+process.chdir(__dirname)
 
-// process.chdir(__dirname)
-//------------------------------------------------------------------------------
-// Tests
-//------------------------------------------------------------------------------
+const ruleTester = new RuleTester({ parser: 'babel-eslint' })
+ruleTester.run('module-resolver', rule, {
+  valid: [
+    "require('actions/api')",
+    "require('reducers/api')",
+    "const { api } = require('actions/api')",
+    "const { api } = require('reducers/api')",
+    "import('actions/api')",
+    "import('reducers/api')",
+    "import { api } from 'reducers/api'",
+    "import { api } from 'reducers/api'",
+    "const { api } = dynamic(import('actions/api'))",
+    "const { api } = dynamic(import('reducers/api'))",
+  ],
 
-var ruleTester = new RuleTester();
-ruleTester.run("module-resolver", rule, {
-
-    valid: [
-
-      "require('../wat')"
-    ],
-
-    invalid: [
+  invalid: [
+    {
+      code: '',
+      errors: [
         {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
-});
+          message: 'Fill me in.',
+          type: 'Me too',
+        },
+      ],
+    },
+  ],
+})
