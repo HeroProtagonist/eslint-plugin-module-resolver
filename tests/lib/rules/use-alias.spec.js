865d8ed22a41575ffb92cff89b7031602c50993a
HeroProtagonist <jstepper91@gmail.com>
2019-12-27 23:57:10 -0500

    run prettier on lib and tests folders

--- tests/lib/rules/use-alias.spec.js
+++ tests/lib/rules/use-alias.spec.js
@@ -6,23 +6,21 @@ const { RuleTester } = require('eslint')
 const rule = require('../../../lib/rules/use-alias')
 const babelConfig = require('../../babelrc')
 
-jest.mock("find-babel-config", () => ({
-  sync: jest.fn(() => ({ config: ({}) })),
-}));
+jest.mock('find-babel-config', () => ({
+  sync: jest.fn(() => ({ config: {} })),
+}))
 
 const projectRoot = '/project'
 let existsSyncSpy
 let cwdSpy
 
 beforeEach(() => {
-  existsSyncSpy = jest.spyOn(fs, 'existsSync').mockImplementation(
-    file => {
-      if (file.includes('lib/parsers.js') || file.includes('lib/parsers/index.js')) {
-        return false
-      }
-      return true
+  existsSyncSpy = jest.spyOn(fs, 'existsSync').mockImplementation(file => {
+    if (file.includes('lib/parsers.js') || file.includes('lib/parsers/index.js')) {
+      return false
     }
-  )
+    return true
+  })
   cwdSpy = jest.spyOn(process, 'cwd').mockImplementation(() => projectRoot)
 })
 
@@ -42,10 +40,10 @@ const createInvalid = (...args) => {
   const defaultErrorMessage = 'Do not use relative path for aliased modules'
 
   if (args.length === 1) {
-    const [ obj ] = args
+    const [obj] = args
     ;({ code, type, filename, options = [], errorMessage } = obj)
   } else {
-    [ code, type, filename, errorMessage ] = args
+    ;[code, type, filename, errorMessage] = args
   }
 
   return {
@@ -76,12 +74,12 @@ describe('with babel config', () => {
       "const { api } = require('reducers/api')",
       "import('actions/api')",
       "import('reducers/api')",
-      "import(`${buildPath}/dist`)",
+      'import(`${buildPath}/dist`)',
       "import { api } from 'actions/api'",
       "import { api } from 'reducers/api'",
       "const { api } = dynamic(import('actions/api'))",
       "const { api } = dynamic(import('reducers/api'))",
-      "const { server } = require(`${buildPath}/dist`)",
+      'const { server } = require(`${buildPath}/dist`)',
       "const { api } = require('./actions/api')",
       "const { api } = require('./reducers/api')",
       "import { api } from './reducers/api'",
@@ -89,51 +87,55 @@ describe('with babel config', () => {
       "const { api } = dynamic(import('./src/client/main'))",
       createInvalid({
         code: "const { api } = dynamic(import('../reducers/api'))",
-        type: "CallExpression",
+        type: 'CallExpression',
         options: [{ ignoreDepth: 1 }],
       }),
       createInvalid({
         code: "import ClientMain from '../../../client/main/components/App'",
-        type: "ImportDeclaration",
+        type: 'ImportDeclaration',
         filename: `${projectRoot}/src/client/main/utils/index.js`,
         options: [{ ignoreDepth: 3 }],
       }),
       createInvalid({
         code: "import('actions/api')",
-        type: "ImportDeclaration",
+        type: 'ImportDeclaration',
         filename: `${projectRoot}/package/one/src/client/main/utils/index.js`,
-        options: [{ projectRoot: "package/one" }],
+        options: [{ projectRoot: 'package/one' }],
       }),
     ],
 
     invalid: [
-      createInvalid("require('../actions/api')", "CallExpression"),
-      createInvalid("require('../reducers/api')", "CallExpression"),
-      createInvalid("import('../../actions/api')", "CallExpression", `${projectRoot}/src/client/index.js`),
-      createInvalid("import('../../reducers/api')", "CallExpression", `${projectRoot}/src/client/index.js`),
-      createInvalid("const { api } = dynamic(import('../actions/api'))", "CallExpression"),
-      createInvalid("import ClientMain from '../../../client/main/components/App'", "ImportDeclaration", `${projectRoot}/src/client/main/utils/index.js`),
-      createInvalid("const { api } = dynamic(import('../reducers/api'))", "CallExpression"),
+      createInvalid("require('../actions/api')", 'CallExpression'),
+      createInvalid("require('../reducers/api')", 'CallExpression'),
+      createInvalid("import('../../actions/api')", 'CallExpression', `${projectRoot}/src/client/index.js`),
+      createInvalid("import('../../reducers/api')", 'CallExpression', `${projectRoot}/src/client/index.js`),
+      createInvalid("const { api } = dynamic(import('../actions/api'))", 'CallExpression'),
+      createInvalid(
+        "import ClientMain from '../../../client/main/components/App'",
+        'ImportDeclaration',
+        `${projectRoot}/src/client/main/utils/index.js`,
+      ),
+      createInvalid("const { api } = dynamic(import('../reducers/api'))", 'CallExpression'),
       createInvalid({
         code: "const { api } = dynamic(import('../reducers/api'))",
-        type: "CallExpression",
+        type: 'CallExpression',
         options: [{ ignoreDepth: 2 }],
       }),
       createInvalid({
         code: "import ClientMain from '../../../client/main/components/App'",
-        type: "ImportDeclaration",
+        type: 'ImportDeclaration',
         filename: `${projectRoot}/src/client/main/utils/index.js`,
         options: [{ ignoreDepth: 1 }],
       }),
       createInvalid({
         code: "import('actions/api')",
-        type: "CallExpression",
+        type: 'CallExpression',
         filename: `${projectRoot}/package/one/src/client/main/utils/index.js`,
-        options: [{ projectRoot: "invalid/project" }],
+        options: [{ projectRoot: 'invalid/project' }],
       }),
       createInvalid({
         code: "import { parseResponse } from '../../../../lib/parsers'",
-        type: "ImportDeclaration",
+        type: 'ImportDeclaration',
         filename: `${projectRoot}/src/client/main/utils/index.js`,
         options: [{ extensions: ['.ts'] }],
       }),
@@ -152,13 +154,13 @@ describe('without babel config', () => {
     invalid: [
       createInvalid({
         code: "require('actions/api')",
-        type: "CallExpression",
-        errorMessage: 'Unable to find config for babel-plugin-module-resolver'
+        type: 'CallExpression',
+        errorMessage: 'Unable to find config for babel-plugin-module-resolver',
       }),
       createInvalid({
         code: "import { api } from './reducers/api'",
-        type: "ImportDeclaration",
-        errorMessage: 'Unable to find config for babel-plugin-module-resolver'
+        type: 'ImportDeclaration',
+        errorMessage: 'Unable to find config for babel-plugin-module-resolver',
       }),
     ],
   })
