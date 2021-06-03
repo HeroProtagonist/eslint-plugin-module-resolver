fb30c482cb5e65615f3478e31b075c57bcdd233b
Ryan Cebulko <ryan@cebulko.com>
2021-06-03 12:54:10 -0400

    Add spec for isAliasPath

---
+++ tests/helpers/is-alias-path.spec.js
@@ -0,0 +1,15 @@
+const isAliasPath = require('../../lib/helpers/is-alias-path');
+
+describe('isAliasPath', () => {
+  it('returns true for aliased module imports', () => {
+    expect(isAliasPath('./src/service', './src/service')).toBe(true);
+  });
+
+  it('returns true for aliased module subpath imports', () => {
+    expect(isAliasPath('./src/service/foo-bar', './src/service')).toBe(true);
+  });
+
+  it('returns false for shared prefix collisions', () => {
+    expect(isAliasPath('./src/services', './src/service')).toBe(false);
+  });
+});
